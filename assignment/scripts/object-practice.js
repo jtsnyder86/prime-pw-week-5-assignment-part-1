3b3b3bde5be2cdbb8c4fa9111652185eeca89d0b
Josh Snyder <joshua.t.snyder@gmail.com>
2022-07-07 22:37:00 -0500

    answered another q

--- assignment/scripts/object-practice.js
+++ assignment/scripts/object-practice.js
@@ -42,7 +42,11 @@ console.log(me.favThreeFoods[2]);
   - Set your object's shoeCount to be what it was plus 1
   - Console.log your updated shoe count. 
 */
+console.log(me.shoeCount);
 
+me.shoeCount = me.shoeCount + 1;
+
+console.log(me.shoeCount); //new shoe count with new pair that Dane bought for me!!
 
 /* 5. Add a new property to an existing object.
   - Add a 'favoriteColor' to your existing object
